80b0a965eb9c0e190e52b477d99965ebba5b4447
Tech-A47 <tech-a47@Tech-A47s-MacBook-Air.local>
2018-04-11 16:13:50 +0100

    first commit

---
+++ faker_example.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+# puts Faker::Name.name
+
+puts "All hell has broken loose. The gatekeeper of Pop culture, #{Faker::LordOfTheRings.character}, is dead.
+Characters from all over the world, from #{Faker::StarWars.planet} to #{Faker::HarryPotter.location} have taken up their arms in an all out battle for global pop culture domination.
+Food is scarce, everyone now lives on #{Faker::Pokemon.name} Pokemon meat to survive.
+#{Faker::Overwatch.hero}, of house #{Faker::GameOfThrones.house} has slain #{Faker::RickAndMorty.character}, of house #{Faker::HarryPotter.house} using #{Faker::Superhero.power}.
+The last words he spoke were '#{Faker::RickAndMorty.quote} #{Faker::Movie.quote} #{Faker::StarWars.wookiee_sentence}'"
